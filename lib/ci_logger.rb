230fe82f505b251118b551ed0d7c9206d922264c
willnet <netwillnet@gmail.com>
2021-09-24 18:44:17 +0900

    Remove meaningless rescue LoadError
    require "rspec/core" doesn't raise error because rspec-core is a dependency of ci_logger

--- lib/ci_logger.rb
+++ lib/ci_logger.rb
@@ -3,34 +3,31 @@ require "ci_logger/railtie"
 require "ci_logger/logger"
 
 module CiLogger
-  begin
-    require "rspec/core"
+  require "rspec/core"
 
-    class Formatter
-      RSpec::Core::Formatters.register self, :example_passed, :example_pending, :example_failed
+  class Formatter
+    RSpec::Core::Formatters.register self, :example_passed, :example_pending, :example_failed
 
-      def initialize(_out)
-        @out = _out
-      end
+    def initialize(_out)
+      @out = _out
+    end
 
-      def example_failed(notification)
-        if Rails.application.config.ci_logger.enabled
-          example = notification.example
-          Rails.logger.debug("finish example at #{example.location}")
-          Rails.logger.sync
-        else
-          Rails.logger.clear
-        end
-      end
-
-      def example_passed(_notification)
-        Rails.logger.clear
-      end
-
-      def example_pending(_notification)
+    def example_failed(notification)
+      if Rails.application.config.ci_logger.enabled
+        example = notification.example
+        Rails.logger.debug("finish example at #{example.location}")
+        Rails.logger.sync
+      else
         Rails.logger.clear
       end
     end
-  rescue LoadError
+
+    def example_passed(_notification)
+      Rails.logger.clear
+    end
+
+    def example_pending(_notification)
+      Rails.logger.clear
+    end
   end
 end
