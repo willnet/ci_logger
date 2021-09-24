b07d0eecef3970b1a4c3043234968f589a6f4c36
willnet <netwillnet@gmail.com>
2021-09-24 19:05:03 +0900

    Move formatter to another file

--- lib/ci_logger.rb
+++ lib/ci_logger.rb
@@ -1,41 +1,7 @@
 require "ci_logger/version"
 require "ci_logger/railtie"
 require "ci_logger/logger"
+require "ci_logger/formatter"
 
 module CiLogger
-  require "rspec/core"
-
-  class Formatter
-    RSpec::Core::Formatters.register self, :example_passed, :example_pending, :example_failed
-
-    def initialize(_out)
-      @out = _out
-    end
-
-    def example_failed(notification)
-      if Rails.application.config.ci_logger.enabled
-        example = notification.example
-        Rails.logger.debug("finish example at #{example.location}")
-        Rails.logger.sync
-      else
-        Rails.logger.sync_with_original_level
-      end
-    end
-
-    def example_passed(_notification)
-      if Rails.application.config.ci_logger.enabled
-        Rails.logger.clear
-      else
-        Rails.logger.sync_with_original_level
-      end
-    end
-
-    def example_pending(_notification)
-      if Rails.application.config.ci_logger.enabled
-        Rails.logger.clear
-      else
-        Rails.logger.sync_with_original_level
-      end
-    end
-  end
 end
